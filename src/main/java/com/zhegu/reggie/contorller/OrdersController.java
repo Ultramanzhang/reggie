ec2f9a89a48f31364619feddc83f73eee4f7b46b
zhegu <zfangyu@163.com>
2022-11-18 16:55:04 +0800

    未优化版本开发完毕

--- src/main/java/com/zhegu/reggie/contorller/OrdersController.java
+++ src/main/java/com/zhegu/reggie/contorller/OrdersController.java
@@ -1,16 +1,32 @@
 package com.zhegu.reggie.contorller;
 
+import com.zhegu.reggie.common.R;
+import com.zhegu.reggie.entity.Orders;
 import com.zhegu.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/order")
 public class OrdersController {
 
     @Autowired
     private OrdersService ordersService;
+
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders){
+
+        ordersService.submit(orders);
+        return null;
+    }
+
+
+
+
+
 }
--- src/main/java/com/zhegu/reggie/service/OrdersService.java
+++ src/main/java/com/zhegu/reggie/service/OrdersService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.zhegu.reggie.entity.Orders;
 
 public interface OrdersService extends IService<Orders> {
+
+    public void submit(Orders orders);
 }
